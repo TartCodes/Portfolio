3a5ea72ac87f6f28354699be9dac402217a18d0b
TartCodes <tartcodes@gmail.com>
2022-06-05 16:09:39 -0400

    still messing with pomo

--- pomodoro/js/main.js
+++ pomodoro/js/main.js
@@ -1,26 +1,22 @@
-const progressBar = document.querySelector(".outerRing"),
-	minElem = document.querySelector("#minutes"),
-	secElem = document.querySelector("#seconds"),
-	startStop = document.querySelector("#stsp"),
-	setting = document.querySelector("#setting"),
-	reset = document.querySelector('#reset'),
-	input = document.querySelector('#input'),
-	button = document.querySelector('#button'),
-	deleteButton = document.querySelector('#delete'),
-	button2 = document.querySelector('#button2')
+const progressBar = document.querySelector(".outerRing")
+const minElem = document.querySelector("#minutes")
+const secElem = document.querySelector("#seconds")
+const startStop = document.querySelector("#stsp")
+const setting = document.querySelector("#setting")
+const reset = document.querySelector('#reset')
 
 
-let minutes = document.querySelector("#minutes").innerHTML,
-	seconds = document.querySelector("#seconds").innerHTML,
-	progress = null,
-	progressStart = 0,
-	progressEnd = parseInt(minutes) * 60 + parseInt(seconds),
-	speed = 1000,
-	degTravel = 360 / progressEnd,
-	toggleSettings = false,
-	secRem = 0,
-	minRem = 0,
-	checkStorage = localStorage.getItem('input') || '|' //keeps local storage
+
+let minutes = document.querySelector("#minutes").innerHTML
+let	seconds = document.querySelector("#seconds").innerHTML
+let	progress = null
+let	progressStart = 0
+let	progressEnd = parseInt(minutes) * 60 + parseInt(seconds)
+let	speed = 1000
+let	degTravel = 360 / progressEnd
+let	toggleSettings = false
+let	secRem = 0
+let	minRem = 0
 
 function progressTrack() {
 	progressStart++;
@@ -125,36 +121,78 @@ secElem.onblur = function () {
 
 //Below code needs fixing
 
-button.addEventListener('click', save)
-deleteButton.addEventListener('click', deleteStorage)
-button2.addEventListener('click', createLi) // temporary show button for debugging
+const input = document.querySelector('#input')
+const addBtn = document.querySelector('#button')
+const deleteButton = document.querySelector('#delete')
+const button2 = document.querySelector('#button2')
 
-let newArr = checkStorage.split('|')
+// let	checkStorage = localStorage.getItem('input') || '|' //keeps local storage
+let	checkStorage = JSON.parse(localStorage.getItem('input')) || []
+
+// addBtn.addEventListener('click', saveToLocalStorage)
+
+addBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	let inputRewardValue = inputReward.value;
+	const newLi = document.createElement('li')
+	newLi.innerHTML = inputRewardValue;
+	userList.append(newLi);
+	inputReward.value = ''
+})
+
+// deleteButton.addEventListener('click', deleteStorage) //removes localstorage data
+
+// button2.addEventListener('click', createLi) // temporary show button for debugging
+
+// let newArr = checkStorage.split('|')
 
 //supposed to create li and append
+// function createLi() {
+// 	let newArr = checkStorage.split('|')
+// 	document.getElementById('createdLi').replaceChildren()	
+// 		for(let i = 1; i < newArr.length - 1; i++){				
+// 			const userList = document.getElementById('createdLi') 
+// 			const makeLi = document.createElement('li')
+// 			makeLi.innerText = newArr[i]
+// 			userList.appendChild(makeLi)
+// 	}	
+//  }
+
+
+//create the li
 function createLi() {
-	document.getElementById('createdLi').replaceChildren()
-	for(let i = 1; i < newArr.length - 1; i++){	
-	const userList = document.getElementById('createdLi') 
-	const makeLi = document.createElement('li')
-	makeLi.innerText = newArr[i]
-	userList.appendChild(makeLi)
-	}	
- }
+	let newArr = checkStorage.split('|'); 
+	const userList = document.getElementById('createdLi')
+	userList.innerHTML = '';
+	let list = newArr.map((item) => `<li>${item}</li>`).join('')
+	userList.insertAdjacentHTML('beforeend', list)
+}
+
 
  //save inputs to local storage
-function save() {		
-	let saveInputs = document.getElementById('input').value	
-	checkStorage += `${saveInputs}|`
-	localStorage.setItem('input', checkStorage)
-	createLi()	
+// function save() {		
+// 	let saveInputs = document.getElementById('input').value	
+// 	checkStorage += `${saveInputs}|`
+// 	localStorage.setItem('input', checkStorage)
+// 	createLi()	
+// }
+
+//adams way of doing it
+function saveToLocalStorage(newInput){
+	checkStorage.push(newInput)
+	localStorage.setItem('input', JSON.stringify(checkStorage))
 }
 
 //removes the stored information 
-function deleteStorage() {	
-	localStorage.removeItem('input', checkStorage)
-	checkStorage = '|'
-}
+// function deleteStorage() {	
+// 	localStorage.removeItem('input', checkStorage)
+// 	checkStorage = '|'
+// }
+
+// function deleteStorage(itemToRemove){
+// 	const index = checkStorage.indexOf(itemToRemove);
+
+// }
 
 
 
