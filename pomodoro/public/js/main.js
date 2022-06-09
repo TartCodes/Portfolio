ba85d783d15ac5a9649854106ed322758318bd73
TartCodes <tartcodes@gmail.com>
2022-06-09 15:52:55 -0400

    working on server and db

--- pomodoro/public/js/main.js
+++ pomodoro/public/js/main.js
@@ -1,22 +1,22 @@
-const progressBar = document.querySelector(".outerRing")
-const minElem = document.querySelector("#minutes")
-const secElem = document.querySelector("#seconds")
-const startStop = document.querySelector("#stsp")
-const setting = document.querySelector("#setting")
-const reset = document.querySelector('#reset')
+/* eslint-disable no-mixed-spaces-and-tabs */
+const progressBar = document.querySelector(".outerRing"),
+	  minElem = document.querySelector("#minutes"),
+	  secElem = document.querySelector("#seconds"),
+	  startStop = document.querySelector("#stsp"),
+	  setting = document.querySelector("#setting"),
+	  reset = document.querySelector('#reset')
+	 
 
-
-
-let minutes = document.querySelector("#minutes").innerHTML
-let	seconds = document.querySelector("#seconds").innerHTML
-let	progress = null
-let	progressStart = 0
-let	progressEnd = parseInt(minutes) * 60 + parseInt(seconds)
-let	speed = 1000
-let	degTravel = 360 / progressEnd
-let	toggleSettings = false
-let	secRem = 0
-let	minRem = 0
+let minutes = document.querySelector("#minutes").innerHTML,
+	seconds = document.querySelector("#seconds").innerHTML,
+	progress = null,
+	progressStart = 0,
+	progressEnd = parseInt(minutes) * 60 + parseInt(seconds),
+	speed = 1000,
+	degTravel = 360 / progressEnd,
+	toggleSettings = false,
+	secRem = 0,
+	minRem = 0
 
 function progressTrack() {
 	progressStart++;
@@ -43,6 +43,8 @@ function progressTrack() {
 	}
 }
 
+
+
 function startStopProgress() {
 	if (!progress) {
 		progress = setInterval(progressTrack, speed);
@@ -56,6 +58,7 @@ function startStopProgress() {
 		  )`;
 	}
 }
+
 
 function resetValues() {
 	if (progress) {
@@ -84,19 +87,20 @@ reset.onclick = () => {
 	document.querySelector('#seconds').innerHTML = seconds 
 }
 
+
 startStop.onclick =  () => {
 	if (startStop.innerHTML === "START") {
-		if (!(parseInt(minutes) === 0 && parseInt(seconds) === 0)) {
-			startStop.innerHTML = "STOP";
-			startStopProgress();
-		} else {
-			alert("Click the Cog and enter a Time Value");
-		}
-	} else {
-		startStop.innerHTML = "START";
+		startStop.innerHTML = "STOP";
 		startStopProgress();
+		return;
+}
+	if (!(parseInt(minutes) === 0 && parseInt(seconds) === 0)) {
+			startStop.innerHTML = "START";
+			startStopProgress();
+			return;	
 	}
-};
+}
+
 
 setting.onclick = () => {
 	if (!toggleSettings) {
@@ -121,78 +125,88 @@ secElem.onblur = function () {
 
 //Below code needs fixing
 
-const input = document.querySelector('#input')
-const addBtn = document.querySelector('#button')
-const deleteButton = document.querySelector('#delete')
-const button2 = document.querySelector('#button2')
 
-// let	checkStorage = localStorage.getItem('input') || '|' //keeps local storage
-let	checkStorage = JSON.parse(localStorage.getItem('input')) || []
 
-// addBtn.addEventListener('click', saveToLocalStorage)
 
-addBtn.addEventListener('click', (e) => {
-	e.preventDefault();
-	let inputRewardValue = inputReward.value;
-	const newLi = document.createElement('li')
-	newLi.innerHTML = inputRewardValue;
-	userList.append(newLi);
-	inputReward.value = ''
-})
 
-// deleteButton.addEventListener('click', deleteStorage) //removes localstorage data
 
-// button2.addEventListener('click', createLi) // temporary show button for debugging
 
-// let newArr = checkStorage.split('|')
 
-//supposed to create li and append
+
+
+
+
+
+
+
+
+// // let	checkStorage = localStorage.getItem('input') || '|' //keeps local storage
+// let	checkStorage = JSON.parse(localStorage.getItem('input')) || []
+
+// // addBtn.addEventListener('click', saveToLocalStorage)
+
+// addBtn.addEventListener('click', (e) => {
+// 	e.preventDefault();
+// 	let inputRewardValue = inputReward.value;
+// 	const newLi = document.createElement('li')
+// 	newLi.innerHTML = inputRewardValue;
+// 	userList.append(newLi);
+// 	inputReward.value = ''
+// })
+
+// // deleteButton.addEventListener('click', deleteStorage) //removes localstorage data
+
+// // button2.addEventListener('click', createLi) // temporary show button for debugging
+
+// // let newArr = checkStorage.split('|')
+
+// //supposed to create li and append
+// // function createLi() {
+// // 	let newArr = checkStorage.split('|')
+// // 	document.getElementById('createdLi').replaceChildren()	
+// // 		for(let i = 1; i < newArr.length - 1; i++){				
+// // 			const userList = document.getElementById('createdLi') 
+// // 			const makeLi = document.createElement('li')
+// // 			makeLi.innerText = newArr[i]
+// // 			userList.appendChild(makeLi)
+// // 	}	
+// //  }
+
+
+// //create the li
 // function createLi() {
-// 	let newArr = checkStorage.split('|')
-// 	document.getElementById('createdLi').replaceChildren()	
-// 		for(let i = 1; i < newArr.length - 1; i++){				
-// 			const userList = document.getElementById('createdLi') 
-// 			const makeLi = document.createElement('li')
-// 			makeLi.innerText = newArr[i]
-// 			userList.appendChild(makeLi)
-// 	}	
-//  }
-
-
-//create the li
-function createLi() {
-	let newArr = checkStorage.split('|'); 
-	const userList = document.getElementById('createdLi')
-	userList.innerHTML = '';
-	let list = newArr.map((item) => `<li>${item}</li>`).join('')
-	userList.insertAdjacentHTML('beforeend', list)
-}
-
-
- //save inputs to local storage
-// function save() {		
-// 	let saveInputs = document.getElementById('input').value	
-// 	checkStorage += `${saveInputs}|`
-// 	localStorage.setItem('input', checkStorage)
-// 	createLi()	
+// 	let newArr = checkStorage.split('|'); 
+// 	const userList = document.getElementById('createdLi')
+// 	userList.innerHTML = '';
+// 	let list = newArr.map((item) => `<li>${item}</li>`).join('')
+// 	userList.insertAdjacentHTML('beforeend', list)
 // }
 
-//adams way of doing it
-function saveToLocalStorage(newInput){
-	checkStorage.push(newInput)
-	localStorage.setItem('input', JSON.stringify(checkStorage))
-}
 
-//removes the stored information 
-// function deleteStorage() {	
-// 	localStorage.removeItem('input', checkStorage)
-// 	checkStorage = '|'
+//  //save inputs to local storage
+// // function save() {		
+// // 	let saveInputs = document.getElementById('input').value	
+// // 	checkStorage += `${saveInputs}|`
+// // 	localStorage.setItem('input', checkStorage)
+// // 	createLi()	
+// // }
+
+// //adams way of doing it
+// function saveToLocalStorage(newInput){
+// 	checkStorage.push(newInput)
+// 	localStorage.setItem('input', JSON.stringify(checkStorage))
 // }
 
-// function deleteStorage(itemToRemove){
-// 	const index = checkStorage.indexOf(itemToRemove);
+// //removes the stored information 
+// // function deleteStorage() {	
+// // 	localStorage.removeItem('input', checkStorage)
+// // 	checkStorage = '|'
+// // }
 
-// }
+// // function deleteStorage(itemToRemove){
+// // 	const index = checkStorage.indexOf(itemToRemove);
+
+// // }
 
 
 
--- pomodoro/server.js
+++ pomodoro/server.js
@@ -1,15 +1,57 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 const PORT = 8001
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+
+MongoClient.connect(
+    "mongodb+srv://TartCodes:<password>@cluster0.roa15t0.mongodb.net/?retryWrites=true&w=majority", { useUnifiedTopology: true }
+  )
+    .then((client) => {
+      console.log("Connected to Database");
+      const db = client.db("pomoRewards");
+      const rewardsCollection = db.collection('rewards')
+
+    
+//=======
+// Middleware
+//=======   
+app.set('view-engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+//=======
+// Routes
+//======= 
+// app.get('/', (req,res) => {
+//     res.sendFile(__dirname + '/index.html')
+// })
 
 
+//ejs
+app.get('/', (req, res) => {
+    db.collection('rewards').find().toArray()  
+      .then(rewards => {
+        res.render('index.ejs', { rewards: rewards })
+      })
+      .catch(/* ... */)
+  })
+
+//   app.post('/rewards', (req, res) => {
+//     rewardsCollection.insertOne(req.body)
+//       .then(result => {
+//         res.redirect('/')
+//       })
+//       .catch(error => console.error(error))
+//   })
 
 
 app.listen(process.env.PORT || PORT , () => {
     //process.env.PORT will default to heroku port first
     console.log(`The server is now running on port ${PORT}`);
 }) 
+
+
+})
