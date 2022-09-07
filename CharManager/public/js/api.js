9ca529558955fae9e36603f0d923ac1e57ed6529
TartCodes <tartcodes@gmail.com>
2022-09-07 16:08:58 -0400

    updates

--- CharManager/public/js/api.js
+++ CharManager/public/js/api.js
@@ -1,13 +1,13 @@
-const myHeaders = new Headers();
-myHeaders.append("Authorization", "da468b89-2bf8-4e2b-a939-79c6e6ef25ce");
+// const myHeaders = new Headers();
+// myHeaders.append("Authorization", "da468b89-2bf8-4e2b-a939-79c6e6ef25ce");
 
-const requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
+// const requestOptions = {
+//   method: 'GET',
+//   headers: myHeaders,
+//   redirect: 'follow'
+// };
 
-fetch("https://api.pathfinder2.fr/v1/pf2/", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+// fetch("https://api.pathfinder2.fr/v1/pf2/", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
